6b139a4acb987575fee6ec0cbc96a0d417e2daf2
HimaniJoshi0 <himanijoshi225@gmail.com>
2023-12-03 14:44:25 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,62 +1,67 @@
-import React, { useState } from 'react'
-import "./App.css"
+import React, { useState , useEffect} from 'react'
 
 const App = () => {
-  const [data, UpdateData] = useState([]);
-  const [task_data, Updatetask_Data] = useState("");
+  const [tododata, setTodoData] = useState([]);
+  const [taskdata, setTaskData] = useState("");
 
   const handle_change = (e) => {
-    Updatetask_Data(e.target.value);
+    setTaskData(e.target.value);
   }
 
-  const submit_data = (e) => {
-    UpdateData([...data, task_data]);
-    Updatetask_Data("");
+  const submit_data = (e) => { 
+    setTodoData([...tododata, taskdata]);
+    localStorage.setItem("tododata", JSON.stringify([...tododata, taskdata]));
+    setTaskData("");  
   }
 
   const delete_task=(index)=>{
-    const newData= [...data];
+    const newData= [...tododata];
     newData.splice(index,1);
-        UpdateData(newData);
+    setTodoData(newData);
   }
+
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem("tododata"));
+    setTodoData(storedData);
+  }, []);
+
+
+  // useEffect(() => {
+  //   const storedData = JSON.parse(localStorage.getItem("tododata"));
+  //   setShowData(storedData);
+  // }, [tododata]);
+
+  const renderData=(item,index)=>{
+    return (
+      <div className='flex bg-white w-70' key={index}>
+        <p>{item}</p>
+        <i class="fa-solid fa-trash" onClick={(index)=>delete_task(index)}></i>
+      </div>  
+  )
+}
+  
 
   return (
     <>
-      <div className='to-do_wrapper'>
-        <div className='to-do'>
+      <div  className="bg-red-200 w-screen h-screen flex flex-col items-center " >
+        <div className='w-80 h-80 flex flex-col items-center bg-blue-400'>
 
-          <div className='heading'>
+          <div className='text-4xl font-bold'>
             <h1>YOUR TO-DO</h1>
           </div>
 
-          <div className='task'>
-            <textarea rows={5} value={task_data} onChange={(e) => handle_change(e)} placeholder='write your task' ></textarea>
-            <button onClick={(e) => submit_data(e)}>Submit</button>
+          <div className=' w-80 flex flex-col '>
+            <textarea rows={5} value={taskdata} onChange={handle_change} placeholder='write your task' ></textarea>
+            <button onClick={submit_data}>Submit</button>
           </div>
 
-
-        {
-        <div className='your_list' >
+          <div className="" >
              <div>
-               <h2>TASK TO-DO</h2>
+               <h2 >TASK TO-DO</h2>
              </div>
-       {data && data.map((item, index) => {
-         return (
-             <div className='list' key={index}>
-               <p>{item}</p>
-               <i class="fa-solid fa-trash" onClick={(index)=>delete_task(index)}></i>
-             </div>  
-         )
-       })
-       }
-       </div>
-
-        }
-
-         
-
-
-
+           {tododata.length && tododata.map(renderData)
+             }
+          </div>
         </div>
       </div>
     </>
